fbabcbc0c38b6dc998a656649ca6d543d3c6e095
Ivaylo <51824320+ivaylokanev@users.noreply.github.com>
2020-07-13 15:36:09 +0300

    Create Libs.h

---
+++ Vectors/Libs.h
@@ -0,0 +1,13 @@
+  
+#ifndef Libs_h
+#define Libs_h
+
+
+#include <iostream>
+#include "exceptions.hpp"
+#include <cmath>
+#include <fstream>
+#define PI 3.14159265359
+using namespace std;
+
+#endif
